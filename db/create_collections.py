42028d03883118971a6649550c9fab2b161bd096
Bruno "Braice" Bricce <62706067+Braicce@users.noreply.github.com>
2024-12-01 23:07:00 -0300

    v6-mongo-init
    Refeita a configuração de criação do banco de dados no Mongo de forma automática pelo Docker Compose

---
+++ db/create_collections.py
@@ -0,0 +1,13 @@
+from db.config import settings
+from db.database import CLIENT
+import random
+
+database = CLIENT[settings.MONGODB_DB]
+
+async def criarCollections():
+
+    existing_collections = await database.list_collection_names()
+    
+    if "messages" not in existing_collections:
+        await database.create_collection("messages")
+        print("A coleção 'chat_app' foi criada e populada com sucesso!")         
